06520a5b0ade418f640ce93f861d105bd4b976f4
Ritviks944 <tritvik24@gmail.com>
2026-06-05 21:56:51 +0530

    Update Google Sheets script URL

--- assist/main.js
+++ assist/main.js
@@ -239,7 +239,7 @@ document.addEventListener('DOMContentLoaded', () => {
         return; 
       }
 
-      const SCRIPT_URL = "REPLACE_WITH_YOUR_NEW_SCRIPT_URL";
+      const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyU_RRSpFcVRWaI6TXy2dsTTbY0cZdgAnR3uA2g4WVtX5mYJe2EkgzqoEMCLHWYbw8r/exec";
 
       try {
         btn.innerHTML = '<i class="fa-solid fa-spinner fa-spin"></i> SENDING...';
